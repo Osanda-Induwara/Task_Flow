9a2710881ac4b6708bb3129ebbff1a73cb605239
Osanda-Induwara <osanda2002@gmail.com>
2026-04-24 23:57:48 +0530

    Implemented User models

---
+++ backend/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcryptjs.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
